5c013bc7bfe4c7560ea79777389604c6cfdd125e
Joel Maxuel <joel.maxuel@gmail.com>
2015-10-12 19:22:18 -0300

    battery: Add fast charging UI support (set 7) - fix derp

--- packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
+++ packages/SystemUI/src/com/android/systemui/BatteryMeterView.java
@@ -896,6 +896,7 @@ public class BatteryMeterView extends View implements DemoMode,
                                     mBoltFrame.left + mBoltPoints[i] * mBoltFrame.width(),
                                     mBoltFrame.top + mBoltPoints[i + 1] * mBoltFrame.height());
                         }
+                        mBoltPath.lineTo(
                                 mBoltFrame.left + mBoltPoints[0] * mBoltFrame.width(),
                                 mBoltFrame.top + mBoltPoints[1] * mBoltFrame.height());
                     }
